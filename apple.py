70dfce7188a307dc93b033bc4a25bd0c233ec40c
Aliya Yusuf Shaikh <aaliyashaikh4013@gmail.com>
2020-07-23 17:57:04 +0530

    Update apple.py

--- apple.py
+++ apple.py
@@ -2,9 +2,9 @@ import random
 from config import Config
 
 class Apple():
-	def __init__(self):
-		self.setNewLocation()
-
-	def setNewLocation(self):
-		self.x = random.randint(0, Config.CELLWDTH - 1)
-		self.y = random.randint(0, Config.CELLHEIGHT -1)
+  def __init__(self):
+    self.setNewLocation()
+  
+  def setNewLocation(self):
+      self.x = random.randint(0, Config.CELLWIDTH - 1)
+      self.y = random.randint(0, Config.CELLHEIGHT - 1)
