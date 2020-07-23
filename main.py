1a5f1b513ab5b836c36c29d8b1ee19b500811612
Aliya Yusuf Shaikh <aaliyashaikh4013@gmail.com>
2020-07-23 17:56:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import sys
-from mygame import Game
+from game import Game
 
 def main():
-	game = Game()
-	game.run()
+  game = Game()
+  game.run()
+  sys.exit()
 
 if __name__ == '__main__':
-	main()
+    main()
 
